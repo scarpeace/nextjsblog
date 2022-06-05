902876c3c00cdd5e9c05148a4c0a0aa78a84549b
Gustavo Scarpellini <scarpellini1992@gmail.com>
2022-06-05 12:20:33 -0300

    finishing featured posts and all posts components/page

---
+++ components/posts/posts-grid.js
@@ -0,0 +1,13 @@
+import PostItem from "./post-item";
+import classes from "./post-grid.module.css";
+
+export default function PostsGrid(props) {
+  const { posts } = props;
+  return (
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem key={post.slug} post={post} />
+      ))}
+    </ul>
+  );
+}
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,1 +1,32 @@
-export default function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-next-js",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Nextjs is the React framework for production it makes building full React apps and sites a breeze and ships with built-in with SSR.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-next-js",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Nextjs is the React framework for production it makes building full React apps and sites a breeze and ships with built-in with SSR.",
+    date: "2022-02-11",
+  },
+  {
+    slug: "getting-started-with-next-js",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Nextjs is the React framework for production it makes building full React apps and sites a breeze and ships with built-in with SSR.",
+    date: "2022-02-12",
+  },
+];
+
+export default function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
